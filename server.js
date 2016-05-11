4ff54b56e46506064ec86d2826d73ce5d9e1ce41
xiaoluoboding <xiaoluoboding@gmail.com>
2016-05-12 00:27:06 +0800

    Code review

--- server.js
+++ server.js
@@ -1,74 +1,64 @@
 var io = require('socket.io')(3000);
-// 连接mongodb
 var conf = require('./config.json');
-var connect = require('./db').connect(conf.db.url, conf.db.options);
+var connect = require('./db').connect(conf.db.uri, conf.db.options);
 
 connect(function(db) {
-    // Get the documents collection
-    var collection = db.collection('mb_user');
+    var collection = db.collection('mb_users');
     var socketid;
     var onlineCount = 0;
 
-    // 更改登录状态
-    function updateOnlineStat(username, stat) {
+    // 设置socketid
+    function setSocketId(username, socketid) {
         collection.update({
             username: username
         }, {
             $set: {
-                online_stat: stat
+                socket_id: socketid
             }
         });
     }
 
-    // 更改socketid
-    function updateSocketId(username, socketid) {
+    // 登出事件
+    function logout(username) {
         collection.update({
             username: username
         }, {
             $set: {
-                socketID: socketid
+                online_stat: false,
+                socket_id: ""
             }
         });
     }
 
-    // 每次重启服务器时,重置用户登录状态和socketid
     collection.find({}).toArray(function(err, docs) {
         for (var i = 0; i < docs.length; i++) {
-            collection.update({
-                username: docs[i].username
-            }, {
-                $set: {
-                    online_stat: false,
-                    socketID: ""
-                }
-            });
+            logout(docs[i].username);
         }
     });
 
     io.on('connection', function(socket) {
         console.log("a user connection");
         socket.on('login', function(obj) {
-            onlineCount++; //在线人数+1
+            onlineCount++;
             console.log(obj.username + "加入! 当前在线人数:" + onlineCount)
             socket.name = obj.username;
-            updateSocketId(obj.username, socket.id);
+            setSocketId(obj.username, socket.id);
         });
 
         // 客户端验证服务端是否启动
-        socket.on('serverOnlineStat', function() {
-            io.emit('serverOnlineStat', {
-                isOnlineStat: true
+        socket.on('checkOnline', function() {
+            io.emit('checkOnline', {
+                isOnline: true
             })
         })
 
         // 接收消息并发送给指定客户端
         socket.on('private message', function(obj) {
-            console.dir(obj);
             collection.find({
                 username: obj.username
             }).toArray(function(err, docs) {
-                if (docs[0].online_stat) { //查询用户是否在线
-                    socketid = docs[0].socketID;
+                if (docs[0].online_stat) {
+                    socketid = docs[0].socket_id;
                     io.sockets.connected[socketid].emit('private message', {
                         title: obj.title,
                         desc: obj.desc,
@@ -95,9 +85,8 @@ connect(function(db) {
                 try {
                     if (docs[0].online_stat) {
                         onlineCount--;
+                        logout(socket.name);
                         console.log(socket.name + "退出! 当前在线人数:" + onlineCount);
-                        updateOnlineStat(socket.name, false);
-                        updateSocketId(socket.name, "");
                     }
                 } catch (e) {
                     // 异常处理
@@ -106,8 +95,9 @@ connect(function(db) {
         });
 
         // 用户退出
-        socket.on('exit', function(obj) {
+        socket.on('logout', function(obj) {
             onlineCount--;
+            logout(obj.username);
             console.log(obj.username + "退出! 当前在线人数:" + onlineCount);
         });
     });
